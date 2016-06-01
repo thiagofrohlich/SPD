54155792ba65629d0c1a5c0d6f95c68f4a537403
Rodrigo Silva <rodrigo.r.silva@outlook.com>
2016-06-01 15:52:54 -0300

    métodos para relatorios

--- spd-web/src/main/java/br/com/spd/rest/RelatorioController.java
+++ spd-web/src/main/java/br/com/spd/rest/RelatorioController.java
@@ -13,6 +13,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,15 +36,92 @@ public class RelatorioController {
 	}
 	
 	@ResponseBody
-	@RequestMapping(value="/test", method=RequestMethod.GET)
-	public byte[] getAll() {
-		Connection conn = DataSourceUtils.getConnection(dataSource);
+	@RequestMapping(value="/alunoTurma/{turma}", method=RequestMethod.GET)
+	public byte[] getAlunoPorTurma(@PathVariable final Long turma) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("turmaDescricao", turma);
+		
+		return geraRelatorio(map, null);
+	}
+	
+	
+	
+	@ResponseBody
+	@RequestMapping(value="/ocorrenciaAluno/{aluno}", method=RequestMethod.GET)
+	public byte[] getOcorrenciaAluno(@PathVariable final Long aluno) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("aluno", aluno);
+		
+		return geraRelatorio(map, null);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/ocorrenciaTurma/{turma}", method=RequestMethod.GET)
+	public byte[] getOcorrenciaTurma(@PathVariable final Long turma) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("turma", turma);
+		
+		return geraRelatorio(map, null);
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/aluno/{aluno}/ocorrencia/{ocorrencia}", method=RequestMethod.GET)
+	public byte[] getAlunoOcorrencia(@PathVariable final Long aluno, @PathVariable final Long ocorrencia) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("aluno", aluno);
+		map.put("ocorrencia", ocorrencia);
+		return geraRelatorio(map, null);
+	}
+	
+	
+	
+	@ResponseBody
+	@RequestMapping(value="/turma/{turma}/avaliacaoAluno/{aluno}", method=RequestMethod.GET)
+	public byte[] getAvaliacaAluno(@PathVariable final Long turma,@PathVariable final Long aluno) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("turma", turma);
+		map.put("aluno", aluno);
+		switch (turma.intValue()) {
+		case 1:
+			return geraRelatorio(map, null);
+		case 2:
+			return geraRelatorio(map, null);
+		case 3:
+			return geraRelatorio(map, null);
+		default:
+			return null;
+			
+		}
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/turma/{turma}", method=RequestMethod.GET)
+	public byte[] getAvaliacaAluno(@PathVariable final Long turma) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("turma", turma);
+		switch (turma.intValue()) {
+		case 1:
+			return geraRelatorio(map, null);
+		case 2:
+			return geraRelatorio(map, null);
+		case 3:
+			return geraRelatorio(map, null);
+		default:
+			return null;
+			
+		}
+	}
+	
+	
+	
+	
+
+	private byte[] geraRelatorio(Map<String, Object> map, String relatorio) {
 		try{
+			Connection conn = DataSourceUtils.getConnection(dataSource);
 			SimpleDateFormat format= new SimpleDateFormat("yyyyMMddHHmmss");  
 			String id = format.format(new Date());
-			Map<String, Object> map = new HashMap<>();
-			map.put("turmaDescricao", "Maternal II");
-			JasperReport pathRxml = JasperCompileManager.compileReport("C:\\Users\\Rodrigo\\Documents\\SPD\\spd-web\\relatorios\\Aluno-por-turma.jrxml");
+			JasperReport pathRxml = JasperCompileManager.compileReport(relatorio);
 			JasperPrint printReport = JasperFillManager.fillReport(pathRxml, map, conn);
 			JasperExportManager.exportReportToPdfFile(printReport,"C:\\Users\\Rodrigo\\Documents"+id+".pdf");
 			File file = new File("C:\\Users\\Rodrigo\\Documents"+id+".pdf");
