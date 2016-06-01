30f65928de88f3b067fb369bdc00c02c44bc2458
thiagofrohlich <thi.frohlich@gmail.com>
2016-06-01 20:38:41 -0300

    Professor new fields

--- spd-domain/src/test/java/br/com/spd/domain/ProfessorDomainBuilder.java
+++ spd-domain/src/test/java/br/com/spd/domain/ProfessorDomainBuilder.java
@@ -5,12 +5,26 @@ public class ProfessorDomainBuilder {
 	private Long id = 1l;
 	private Pessoa pessoa = null;
 	private Turma turma = null;
-	
+	private String cep = "teste-cep";
+	private String rua = "teste-rua";
+	private String numero = "teste-numero";
+	private String bairro = "teste-bairro";
+	private String cidade = "teste-cidade";
+	private String estado = "teste-estado";
+	private String email = "teste-email";
+
 	public Professor build() {
 		Professor professor = new Professor();
 		professor.setId(id);
 		professor.setPessoa(pessoa);
 		professor.setTurma(turma);
+		professor.setCep(cep);
+		professor.setRua(rua);
+		professor.setNumero(numero);
+		professor.setBairro(bairro);
+		professor.setCidade(cidade);
+		professor.setEstado(estado);
+		professor.setEmail(email);
 		return professor;
 	}
 
@@ -26,6 +40,41 @@ public class ProfessorDomainBuilder {
 
 	public ProfessorDomainBuilder withTurma(Turma turma) {
 		this.turma = turma;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withCep(String cep) {
+		this.cep = cep;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withRua(String rua) {
+		this.rua = rua;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withNumero(String numero) {
+		this.numero = numero;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withBairro(String bairro) {
+		this.bairro = bairro;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withCidade(String cidade) {
+		this.cidade = cidade;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withEstado(String estado) {
+		this.estado = estado;
+		return this;
+	}
+
+	public ProfessorDomainBuilder withEmail(String email) {
+		this.email = email;
 		return this;
 	}
 	
